641fc05bb212f653ee4b85ec52af2e5272355931
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2023-12-06 23:27:41 +0100

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,35 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 19:09:07 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/12/06 22:37:49 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/12/06 23:23:22 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
 
-// format = "hamza %d %s" , 1 , "man";
-
+int chek_format(const char *format, va_list argptr,  int i)
+{
+			int c;
+			c = 0;
+			if(format[i] == '%')
+				c += (ft_putchar('%'));
+			else if (format[i] == 's')
+				c += (ft_putstr(va_arg(argptr, char *)));
+			else if (format[i] == 'c')
+				c += (ft_putchar(va_arg(argptr, int)));
+			else if (format[i] == 'd' || format[i] == 'i')
+				c +=(ft_putdes(va_arg(argptr, int)));
+			else if (format[i] == 'x' || format[i] == 'X')
+				c += (ft_puthex(va_arg(argptr, int), format[i]));
+			else if (format[i] == 'u')
+				c += (ft_put_unsigned_des(va_arg(argptr, int)));
+			else if (format[i] == 'p')
+				c += (ft_putadd(va_arg(argptr, void *)));
+			else
+				c += ft_putchar(format[i]);
+			return c;
+}
 
 int	ft_printf(const char *format , ...)
 {
@@ -31,31 +51,15 @@ int	ft_printf(const char *format , ...)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-				break;
-			if(format[i + 1] == '%')
-				count = count + ft_putchar('%');
-			else if (format[i + 1] == 's')
-				count = count + ft_putstr(va_arg(argptr, char *));
-			else if (format[i + 1] == 'c')
-				count = count + ft_putchar(va_arg(argptr, int));
-			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-				count = count + ft_putdes(va_arg(argptr, int));
-			else if (format[i + 1] == 'x' || format[i + 1] == 'X')
-				count = count + ft_puthex(va_arg(argptr, int), format[i + 1]);
-			else if (format[i + 1] == 'u')
-				count = count + ft_put_unsigned_des(va_arg(argptr, int));
-			else if (format[i + 1] == 'p')
-				count += ft_putadd(va_arg(argptr, void *));
-				
 			i++;
+			if (format[i] == '\0')
+				break;
+			count = count +  chek_format(format,argptr, i);
 		}
 		else
-		{
-			ft_putchar(format[i]);
-			count = count + 1;
-		}
+			count += ft_putchar(format[i]);
 		i++;
+			
 	}
 	va_end(argptr);
 	return (count);
@@ -63,8 +67,8 @@ int	ft_printf(const char *format , ...)
 // #include <stdio.h>
 // int main()
 // {
-// 	//printf("%");
-// 	ft_printf("%");
+// 	 printf("%");
+// 	 ft_printf("%");
 
 	
 // }
