5bd5790e6f62f6b2de0878d4129d354fa4c19f43
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2023-12-12 22:40:26 +0100

    update

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:22:02 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/12/10 21:16:28 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/12/12 22:38:56 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,20 @@ int	ft_puthex(unsigned int n, char x)
 		c += ft_putchar('A' + remainder - 10);
 	return (c);
 }
+//0123456789abcdef
+//0123456789ABCDEF
+int	ft_puthex1(unsigned int n, char x)
+{
+	char *p;
+	int i = 0;
+	if(x = 'x')
+		p = "0123456789abcdef";
+	else if(x = 'X')
+	    p = "0123456789ABCDEF";
+	
+	if(n >= 16)
+		i += ft_puthex1(n/16, x);
+	i += ft_putchar(p[n%16]);
+	return (i);
+}
+//char **ft_split(char *s)
--- main.c
+++ main.c
@@ -6,16 +6,33 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 11:56:58 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/12/12 12:17:59 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/12/12 13:09:54 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdarg.h>
 #include <stdio.h>
+int sum(int n, ...)
+{
+	int sum,i;
 
+	i = 0;
+	sum = 0;
+	va_list ptr;
+	va_start(ptr, n);
+	while (i < n)
+	{
+		sum += va_arg(ptr, int);
+		i++;
+	}
+	va_end(ptr);
+	return (sum);
+}
 int main()
 {
 	int a;
 	a = 5;
-	ft_printf("%s\n%s", "dfsf", "fdafasf");
+	printf("%d", sum(5, 2, 2, 2, 2, 2));
+	return (0);
 }
